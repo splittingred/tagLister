9d12b3a228c2151726606e6a8619e352b7bf5337
Shaun McCormick <shaun@modxcms.com>
2010-06-14 17:18:51 -0500

    Preparing 1.0.0 release

--- _build/data/transport.snippets.php
+++ _build/data/transport.snippets.php
@@ -9,25 +9,13 @@ $snippets = array();
 $snippets[1]= $modx->newObject('modSnippet');
 $snippets[1]->fromArray(array(
     'id' => 1,
-    'name' => 'Archivist',
-    'description' => '',
-    'snippet' => getSnippetContent($sources['elements'].'snippets/snippet.archivist.php'),
+    'name' => 'tagLister',
+    'description' => 'A simple tag listing snippet that grabs tags from a TV value.',
+    'snippet' => getSnippetContent($sources['elements'].'snippets/snippet.taglister.php'),
 ),'',true,true);
-$properties = include $sources['properties'].'properties.archivist.php';
+$properties = include $sources['properties'].'properties.taglister.php';
 //$properties = adjustProperties($modx,$properties,$sources['lexicon']);
 $snippets[1]->setProperties($properties);
-unset($properties);
-
-$snippets[2]= $modx->newObject('modSnippet');
-$snippets[2]->fromArray(array(
-    'id' => 2,
-    'name' => 'getArchives',
-    'description' => '',
-    'snippet' => getSnippetContent($sources['elements'].'snippets/snippet.getarchives.php'),
-),'',true,true);
-$properties = include $sources['properties'].'properties.getarchives.php';
-//$properties = adjustProperties($modx,$properties,$sources['lexicon']);
-$snippets[2]->setProperties($properties);
 unset($properties);
 
 return $snippets;
--- core/components/taglister/docs/readme.txt
+++ core/components/taglister/docs/readme.txt
@@ -6,7 +6,9 @@ Created: June 15th, 2010
 Author: Shaun McCormick <shaun@modxcms.com>
 License: GNU GPLv2 (or later at your option)
 
-This is a simple tag listing snippet. Please see the documentation at:
+A simple tag listing snippet that grabs tags from a TV value.
+
+Please see the documentation at:
 http://docs.modxcms.com/display/ADDON/tagLister/
 
 Thanks for using tagLister!
