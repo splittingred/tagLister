f71a89d7a37a3ba264e3714946a9dfba3ffa9d3d
Shaun McCormick <shaun@modxcms.com>
2010-06-14 16:39:39 -0500

    Initial commit.

---
+++ _build/includes/functions.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * tagLister
+ *
+ * Copyright 2010 by Shaun McCormick <shaun@modxcms.com>
+ *
+ * This file is part of tagLister, a simple tag listing snippet for MODx
+ * Revolution.
+ *
+ * tagLister is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * tagLister is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * tagLister; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package taglister
+ */
+/**
+ * @package archivist
+ * @subpackage build
+ */
+function getSnippetContent($filename) {
+    $o = file_get_contents($filename);
+    $o = str_replace('<?php','',$o);
+    $o = str_replace('?>','',$o);
+    $o = trim($o);
+    return $o;
+}
+
+/* Due to a bug in Revo RC-2, lexicon-based properties cannot be done.
+ * To workaround this until RC-3, auto-translate them to en here.
+ */
+function adjustProperties($modx,$properties = array(),$lexiconDir = false) {
+    $_lang = array();
+    if (empty($lexiconDir)) return $_lang;
+    include $lexiconDir.'en/properties.inc.php';
+
+    $newProperties = array();
+    foreach ($properties as $property) {
+        $property['desc'] = $_lang[$property['desc']];
+        unset($property['lexicon']);
+        $newProperties[] = $property;
+    }
+    return $newProperties;
+}
---
+++ core/components/taglister/docs/readme.txt
@@ -0,0 +1,14 @@
+--------------------
+Snippet: tagLister
+--------------------
+Version: 1.0.0
+Created: June 15th, 2010
+Author: Shaun McCormick <shaun@modxcms.com>
+License: GNU GPLv2 (or later at your option)
+
+This is a simple tag listing snippet. Please see the documentation at:
+http://docs.modxcms.com/display/ADDON/tagLister/
+
+Thanks for using tagLister!
+Shaun McCormick
+shaun@modxcms.com
